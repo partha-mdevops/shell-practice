a3e9db054b4fcc10e7dc64524716453179bd5530
Partha <partha.mtsl@gmail.com>
2026-01-29 14:38:00 +0530

    somechanged happened

---
+++ 03-variable.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+PERSON1=Trump
+PERSON2=Putin
+echo "$PERSON1:  Hello $PERSON2, how are you?"
+echo "$PERSON2:  Hi $PERSON1, i am doing good, how are you."
+echo "$PERSON1:  which course you are learning now?"
+echo "$PERSON2:  I am learning devops in Joindevops"
