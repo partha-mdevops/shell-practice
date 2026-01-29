3f71463567419567197879040e672f332d54d109
Partha <partha.mtsl@gmail.com>
2026-01-29 14:25:12 +0530

    somechanged happened

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "Suresh:  Hello Ramesh, how are you?"
+echo "Ramesh:  Hi Suresh, i am doing good, how are you."
+echo "Suresh:  which course you are learning now?"
+echo "Ramesh:  I am learning devops in Joindevops"
